2117397417d4ef2c2fef121a186bce963c2f00e0
eqj619 <eqj619@gmail.com>
2020-08-19 15:16:58 -0700

    get jira data

--- jira_team_productivity.py
+++ jira_team_productivity.py
@@ -1,4 +1,38 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import pandas as pd
 import datetime
+
+#
+# Get the number of refult of search with filter
+#
+class jiraTrendFilter:
+    url = "https://test.atlassian.net/rest/api/2/search"
+    auth = ""
+    headers = {
+       "Accept": "application/json"
+    }
+
+    def __init__(self, jsonJiraAccess):
+        self.url = jsonJiraAccess['url']
+        self.auth = HTTPBasicAuth(jsonJiraAccess['user'], jsonJiraAccess['key'])
+
+    def GetNumOfJiraFilter(self, JiraFilter):
+        query = {
+           #'jql': 'project = GDP AND issuetype in (Epic, Story, Task) AND created >= 2020-07-01 AND created <= 2020-07-31'
+           'jql': JiraFilter
+        }
+        response = requests.request(
+           "GET",
+           self.url,
+           headers = self.headers,
+           params = query,
+           auth = self.auth
+        )
+
+        df = pd.read_json(response.text)
+        return(len(df))
 
 def last_day_of_month(any_day):
     next_month = any_day.replace(day=28) + datetime.timedelta(days=4)  # this will never fail
@@ -9,20 +43,35 @@ def createDaysList(daysList, monthList):
     from datetime import date
 
     today = date.today()
-    i = 0
-    for y in range(today.year-1, today.year+1):
+    for y in range(today.year-2, today.year):
         for m in range(today.month, today.month+12):
-            m = m%12
-            if m == 0:
-                m = 12
-            firstday = datetime.date(y, m, 1)
+            mm = m%12
+            if mm == 0:
+                mm = 12
+                y += 1
+            firstday = datetime.date(y, mm, 1)
             lastday = last_day_of_month(firstday)
-            #print(firstday, ", ", lastday)
             daysList.append([firstday.strftime("%Y-%m-%d"), lastday.strftime("%Y-%m-%d")])
             monthList.append(firstday.strftime("%Y-%m"))
-
 
 # main
 daysList = []
 monthList = []
 createDaysList(daysList, monthList)
+
+json_open = open('jira_filter.json', 'r')
+json_load = json.load(json_open)
+
+tf = jiraTrendFilter(json_load['jiraAccess'])
+
+projectName = json_load['input']['project']
+for day in daysList:
+    f = 'project in (' + projectName + ') AND issuetype in (Epic, Story, Task) AND created >= ' \
+        + day[0] +  ' AND created <= ' + day[1]
+    createdTickets = tf.GetNumOfJiraFilter(f)
+    print(createdTickets, " >>> ", f)
+
+    f = 'project in (' + projectName + ') AND issuetype in (Epic, Story, Task) AND resolved >= ' \
+        + day[0] +  ' AND resolved <= ' + day[1]
+    createdTickets = tf.GetNumOfJiraFilter(f)
+    print(createdTickets, " >>> ", f)
