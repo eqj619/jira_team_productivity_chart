bc9128eaafe0af894c82fa5cfe90f2dd38c739a6
eqj619 <eqj619@gmail.com>
2020-08-20 15:30:35 -0700

    move jira access class to libray file

--- jira_team_productivity.py
+++ jira_team_productivity.py
@@ -1,39 +1,8 @@
-import requests
-from requests.auth import HTTPBasicAuth
+import jiralibrary as jlib
 import json
 import pandas as pd
 import datetime
 import openpyxl
-
-#
-# Get the number of refult of search with filter
-#
-class jiraTrendFilter:
-    url = "https://test.atlassian.net/rest/api/2/search"
-    auth = ""
-    headers = {
-       "Accept": "application/json"
-    }
-
-    def __init__(self, jsonJiraAccess):
-        self.url = jsonJiraAccess['url']
-        self.auth = HTTPBasicAuth(jsonJiraAccess['user'], jsonJiraAccess['key'])
-
-    def GetNumOfJiraFilter(self, JiraFilter):
-        query = {
-           #'jql': 'project = GDP AND issuetype in (Epic, Story, Task) AND created >= 2020-07-01 AND created <= 2020-07-31'
-           'jql': JiraFilter
-        }
-        response = requests.request(
-           "GET",
-           self.url,
-           headers = self.headers,
-           params = query,
-           auth = self.auth
-        )
-
-        df = pd.read_json(response.text)
-        return(len(df))
 
 def last_day_of_month(any_day):
     next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
@@ -81,19 +50,19 @@ except:
     wb.close()
     exit(0)
 
-tf = jiraTrendFilter(json_load['jiraAccess'])
+jf = jlib.DoFilter(json_load['jiraAccess'])
 
 projectName = json_load['input']['project']
 i = 0
 for day in daysList:
     f = 'project in (' + projectName + ') AND issuetype in (Epic, Story, Task) AND created >= ' \
         + day[0] +  ' AND created <= ' + day[1]
-    ws.cell(row = i+2, column = 2).value = tf.GetNumOfJiraFilter(f)
+    ws.cell(row = i+2, column = 2).value = jf.GetNumOfJiraFilter(f)
     print(" >>> ", f)
 
     f = 'project in (' + projectName + ') AND issuetype in (Epic, Story, Task) AND resolved >= ' \
         + day[0] +  ' AND resolved <= ' + day[1]
-    ws.cell(row = i+2, column = 4).value = tf.GetNumOfJiraFilter(f)
+    ws.cell(row = i+2, column = 4).value = jf.GetNumOfJiraFilter(f)
     print(" >>> ", f)
     i += 1
 
---
+++ jiralibrary.py
@@ -0,0 +1,36 @@
+import time
+from datetime import date
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+import pandas as pd
+
+def test_func(name):
+  print("My name is " + name + ".")
+  print(date.today())
+
+class DoFilter:
+    url = "https://test.atlassian.net/rest/api/2/search"
+    auth = ""
+    headers = {
+       "Accept": "application/json"
+    }
+
+    def __init__(self, jsonJiraAccess):
+        self.url = jsonJiraAccess['url']
+        self.auth = HTTPBasicAuth(jsonJiraAccess['user'], jsonJiraAccess['key'])
+
+    def GetNumOfJiraFilter(self, filter):
+        query = {
+           #'jql': 'project = GDP AND issuetype in (Epic, Story, Task) AND created >= 2020-07-01 AND created <= 2020-07-31'
+           'jql': filter
+        }
+        response = requests.request(
+           "GET",
+           self.url,
+           headers = self.headers,
+           params = query,
+           auth = self.auth
+        )
+        df = pd.read_json(response.text)
+        return(len(df))
